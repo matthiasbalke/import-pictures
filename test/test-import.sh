952b272f9e26a783e4daa6554add40ef014c9c4b
Matthias Balke <Matthias.Balke@googlemail.com>
2025-06-05 10:41:43 +0200

    add test script

--- test/test-import.sh
+++ test/test-import.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+SCRIPT_DIR=$(dirname "$0")
+
 echo ">> resetting test bed..."
-rm -rf import || true
-cp -a test/import-template import || true
+rm -rf ${SCRIPT_DIR}/../import || true
+cp -a ${SCRIPT_DIR}/import-template ${SCRIPT_DIR}/../import || true
 echo "done."
 echo ""
 
-import-pictures.sh
+${SCRIPT_DIR}/../import-pictures.sh
