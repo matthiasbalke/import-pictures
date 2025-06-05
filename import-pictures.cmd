9308b82ed29411f06c581ddea21119934c1fce77
Matthias Balke <Matthias.Balke@googlemail.com>
2025-06-05 10:04:41 +0200

    add windows wrapper

---
+++ import-pictures.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+REM run script to prepare images for sorting
+docker run \
+  --mount type=bind,src=%~dp0/import,dst=/source \
+  --mount type=bind,src=%~dp0/process-images.sh,dst=/process-images.sh \
+  --rm -it ghcr.io/matthiasbalke/imagemagick:0.1.1-7.1.1.13-r1
+
+REM sort images
+docker run \
+  --mount type=bind,src=%~dp0/import,dst=/source \
+  --mount type=bind,src=%~dp0,dst=/target \
+  --rm -it ghcr.io/matthiasbalke/sortphotos:0.1.1 \
+  --test --recursive --sort %Y-%m-%d /source /target
+
+pause
